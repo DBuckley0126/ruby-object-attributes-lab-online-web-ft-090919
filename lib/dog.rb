fe86ac94bccd7612699cc8960be9f7cbcda771b1
Danny Buckley <dannyboy.msn@hotmail.com>
2019-09-19 15:19:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
-  def initialize(name)
-    @name = name
-  end
+  #def initialize(name)
+  #  @name = name
+  #end
 
-  def name=(name)
-    @name = name
+  def name=(dog_name)
+    @name = dog_name
   end
 
   def name
